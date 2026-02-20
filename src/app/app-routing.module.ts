89b0c44f7caa28fcc9c36da4e1db8f60ee790099
najaryasmine <yasmine.najar@esprit.tn>
2026-02-20 14:55:33 +0100

    workshop 3 maj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 import { NotfoundComponent } from './core/notfound/notfound.component';
-import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
-import { SuggestionDetailsComponent } from './core/suggestion-details/suggestion-details.component';
+import { ListSuggestionComponent } from './features/suggestions/list-suggestion/list-suggestion.component';
+import { SuggestionDetailsComponent } from './features/suggestions/suggestion-details/suggestion-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'listSuggestion', component: ListSuggestionComponent },
   { path: 'suggestion/:id', component: SuggestionDetailsComponent },
+  { path: 'suggestions', loadChildren: () => import('./features/suggestions/suggestions.module').then(m => m.SuggestionsModule) },
+  { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) },
   { path: '**', component: NotfoundComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Pour le two-way binding
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 import { HomeComponent } from './core/home/home.component';
 import { NotfoundComponent } from './core/notfound/notfound.component';
 import { RouterModule } from '@angular/router';
-import { SuggestionDetailsComponent } from './core/suggestion-details/suggestion-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListSuggestionComponent,
     HomeComponent,
-    NotfoundComponent,
-    SuggestionDetailsComponent
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,  // pour [(ngModel)]
-    RouterModule
-    
-
+    FormsModule,
+    RouterModule  // ← nécessaire pour routerLink dans HeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
